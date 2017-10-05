a8857dbe58d13ab515e57f8514064fe2191f42f0
Tsubasa Nakayama <app.nakayama@gmail.com>
2017-10-05 22:22:59 +0900

    Initial Commit

---
+++ cppp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppp
+//
+//  Created by 中山翼 on 2017/10/05.
+//  Copyright © 2017年 Tsubasa Nakayama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
